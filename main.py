226a9d0ad1e80691b7747234d596c327dc779377
moameed <mchafik2006@gmail.com>
2025-02-11 00:49:08 +0100

    home page

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from flask import Flask
-
+from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
 def search():
-    return ""
+    return render_template('index.html')
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
