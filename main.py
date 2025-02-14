7e0c6f31a3b65668d73366a266f24f4735b7800c
moameed <mchafik2006@gmail.com>
2025-02-14 17:55:11 +0100

    change

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+import json
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/",methods=['GET', 'POST'])
 def search():
     return render_template('index.html')
 
-
+def fetch():
+    data = request.json
+    url = 'https://www.omdbapi.com/?apikey=9e9808e5&t=oppenheimer&plot=full' 
 
 if __name__ == "__main__":
     app.run(debug=True)
